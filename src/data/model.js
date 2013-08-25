257271254f7121904bcc555d6477ad68c458adba
unknown <Fahim@Fahim-PC.(none)>
2013-08-25 12:50:08 +0100

    init

---
+++ src/data/model.js
@@ -0,0 +1,49 @@
+var Model = {
+
+	data : null,
+	url : {
+		settings : "settings.json",
+		data : "data.json"
+	},
+	currentSection : 1,
+	template : {
+		index : 0,
+		colorIndex : 0,
+	},
+	page : {
+		index:0,
+		content:
+		[
+		
+		]
+	},
+	className : {
+		leftThumb : "leftThumb",
+		page : "page",
+		formItem : "formItem",
+		color : "color"
+	},
+	id : {
+		thumbHolder : "thumbHolder",
+		backout : "backout",
+		messageHolder : "messageHolder",
+		message : "message",
+		rightThumbHolder : "rightThumbHolder",
+		rightColors : "rightColors",
+		left : "left",
+		right : "right",
+		next : "next",
+		pageHolder : "pageHolder",
+		formItem : "formItem-",
+		filePrevious : "filePrevious-",
+		formHolder : "formHolder",
+		pageGreyTitle : "pageGreyTitle",
+		back : "back",
+		rightThumb : "rightThumb"
+	},
+	att : {
+		templateIndex : "template-index",
+		pageIndex : "page-index",
+		colorIndex : "color-index"
+	}
+}
---
+++ src/view/view.js
@@ -0,0 +1,45 @@
+var View=
+{
+	init:function()
+	{
+		this.setSection();
+	},
+	setSection:function()
+	{
+		switch(Model.currentSection)
+		{
+			case 1:
+			TemplateView.init();
+			break;
+			case 2:
+			ContentView.init();		
+			break;
+		}
+	},
+	changeCurrentSection:function(index)
+	{
+		//console.log(index);
+		var leftId = Model.id.left+Model.currentSection;
+		var rightId = Model.id.right+Model.currentSection;
+		document.getElementById(leftId).style.display="none";
+		document.getElementById(rightId).style.display="none";
+		Model.currentSection=index;
+		leftId = Model.id.left+Model.currentSection;
+		rightId = Model.id.right+Model.currentSection;
+		document.getElementById(leftId).style.display="block";
+		document.getElementById(rightId).style.display="block";
+		this.setSection();
+	},
+	showMessage:function(value)
+	{
+		document.getElementById(Model.id.backout).style.height=document.body.offsetHeight+"px";
+		document.getElementById(Model.id.backout).style.display ="block";
+		document.getElementById(Model.id.messageHolder).style.display ="block";
+		document.getElementById(Model.id.message).innerHTML=value;
+	},
+	hideMessage:function()
+	{
+		document.getElementById(Model.id.backout).style.display ="none";
+		document.getElementById(Model.id.messageHolder).style.display ="none";
+	}
+}
